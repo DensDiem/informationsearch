2ace845b2f0ec5ff2b5c357a06bc1574224f9a29
FloYashiro <32176248+FloYashiro@users.noreply.github.com>
2018-02-05 01:49:35 +0200

    practice 3

--- main.rb
+++ main.rb
@@ -1,11 +1,16 @@
+$num_of_doc = 12
 def collection_to_a()
+
   collection = []
   i = 1
-  while i <= 12
+  while i <= $num_of_doc
   filename ="file_"+i.to_s+".txt"
     file = File.new(filename)
     file.each do |line|
-      line = line.delete "/?/!/,— .-»…"
+      line = line.delete "/?/<!/,— .-»…–№“”"
+      line = line.delete '"'
+      line = line.delete ")(+-"
+      line = line.split
       line = line.split
       j = 0
       while j < line.size
@@ -63,7 +68,7 @@ def create_matrix(indx)
   while indx.size > i
     arrays.push indx[i][0]
     j=1
-    while j <= 12
+    while j <= $num_of_doc
       if indx[i].include?(j)
         arrays.push(1)
       else
@@ -79,7 +84,7 @@ def create_matrix(indx)
 end
 def check(answer,matrix)
   i = 0
-  while i < 12
+  while i < $num_of_doc
     answer[i] = answer[i]&matrix[i]
     i+=1
   end
@@ -88,14 +93,20 @@ end
 def boolean_search_simple(word, matrix)
   word = word.delete " "
   word = word.split "AND"
-  answer = [1,1,1,1,1,1,1,1,1,1,1,1]
+  answer = []
+  i = 0
+  f = true
+  while i < $num_of_doc
+  answer.push(1);
+    i+=1
+  end
   j=0
   while j < word.size
   i = 0
   while i < matrix.size
     if matrix[i][0] == word[j]
       f=false
-      answer = check(answer,matrix[i][1..12])
+      answer = check(answer,matrix[i][1..$num_of_doc])
     end
     if i == matrix.size && matrix[i][0] == word[j]
       return "not found"
@@ -107,10 +118,10 @@ def boolean_search_simple(word, matrix)
   end
   k=0
   doc = []
-  if answer == [1,1,1,1,1,1,1,1,1,1,1,1]
-    return "not found"
-  end
-  while k < 12
+if f
+  return "not found"
+end
+  while k < $num_of_doc
     if answer[k] == 1
       doc.push(k+1)
     end
@@ -119,11 +130,106 @@ def boolean_search_simple(word, matrix)
      return doc
 
 end
+def collection_to_two_word()
 
+  collection = []
+  i = 1
+  while i <= $num_of_doc
+    filename ="file_"+i.to_s+".txt"
+    file = File.new(filename)
+    file.each do |line|
+      line = line.delete "/?/</-/(/!’"
+      line = line.delete '"'
+      line = line.delete ")+-"
+      line = line.split
+      j = 0
+      while j < line.size
+        collection.push( [line[j].to_s+ '*' +line[j+1].to_s,i])
+        j+=1
+      end
+    end
+    i+=1
+  end
+  return collection
+end
 
+def boolean_search_two_word(word, matrix)
+
+  words = word.split " "
+  answer = []
+  i = 0
+  f = true
+  while i < $num_of_doc
+    answer.push(1);
+    i+=1
+  end
+  j=0
+  while j < words.size-1
+    word = words[j].to_s+'*'+ words[j+1].to_s
+    i = 0
+
+    while i < matrix.size
+      m = matrix[i][0]
+      if matrix[i][0] == word
+        f=false
+        answer = check(answer,matrix[i][1..$num_of_doc])
+        break
+      else
+      k = i
+      l = matrix.size
+      l = matrix[i][0]
+      if i == matrix.size-1 && matrix[i][0] != word
+        return "not found"
+      end
+      end
+      i+=1
+
+    end
+    j+=1
+  end
+  k=0
+  doc = []
+  if f
+    return "not found"
+  end
+  while k < $num_of_doc
+    if answer[k] == 1
+      doc.push(k+1)
+    end
+    k+=1
+  end
+  return doc
+
+end
+
+def collection_to_coord()
+
+  collection = []
+  i = 1
+  while i <= $num_of_doc
+    filename ="file_"+i.to_s+".txt"
+    file = File.new(filename)
+    file.each do |line|
+      line = line.delete "/?/</-/(/!’"
+      line = line.delete '"'
+      line = line.delete ")+-"
+      line = line.split
+      j = 0
+      k = 1
+      while j < line.size
+        collection.push( [line[j],i,k+=1])
+        j+=1
+      end
+    end
+    i+=1
+  end
+  return collection
+end
 #test
-myCol = collection_to_a
-get_wordbook_txt("wordbook3", myCol)
+
+#myCol = collection_to_a
+myCol = collection_to_two_word
+get_wordbook_txt("wordbook", myCol)
 indx =  create_index(myCol.uniq)
 i = 0
 puts "inverted index"
@@ -142,4 +248,7 @@ while i < matrix.size
 end
 
 puts "boolean_search, ID doc"
-print boolean_search_simple("додому AND ви", matrix)
+print boolean_search_two_word("У кінці коридору ліворуч були сходи", matrix)
+
+myCol =  collection_to_coord()
+get_wordbook_txt("wordbook2", myCol)
